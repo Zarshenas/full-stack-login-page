bcc21e113e208a3126ed5208275e5606607646d6
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2024-01-06 14:16:18 +0330

    constant splited

---
+++ BackEnd/constants/userdb.js
@@ -0,0 +1,10 @@
+const dataBase = {
+    users: [
+      { id: 1, name: "ali", password: "e_@ggd" },
+      { id: 2, name: "mehdi", password: "12345" },
+      { id: 3, name: "milad", password: "shmrd" },
+    ],
+  };
+module.exports={
+    userDB:dataBase
+}
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,27 +1,24 @@
 const http = require("http");
 const url = require("node:url");
-const fs = require("node:fs");
-const dataBase = {
-  users: [
-    { id: 1, name: "ali", password: "e_@ggd" },
-    { id: 2, name: "reza", password: "12ws$#f" },
-    { id: 3, name: "ahmed", password: "shmrd#%%%321" },
-  ],
-};
+
+const {userDB} = require('./constants/userdb');
 
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin","*")
-
+  
   const parsedURL = url.parse(req.url, true);
+  const mmd = userDB.users.find(
+    (user) => user.password === parsedURL.query.password
+  )
 
   if (parsedURL.pathname === "/api/v1/users") {
     if(!parsedURL.query.username){
-      res.write(JSON.stringify(dataBase.users));
+      res.write(JSON.stringify(userDB.users));
     }else{
-      const username = dataBase.users.find(
+      const username = userDB.users.find(
         (user) => user.name.toLowerCase() === parsedURL.query.username.toLowerCase()
       );
-      const pass = dataBase.users.find(
+      const pass = userDB.users.find(
         (user) => user.password === parsedURL.query.password
       );
       if (username && pass) {
